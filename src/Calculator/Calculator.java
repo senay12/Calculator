107ecb887a59c51723db7d08919cecbe004dedd6
senay12 <46760944+senay12@users.noreply.github.com>
2019-01-25 21:53:08 -0600

    update multiply

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator{
         return num1-num2;
 
     }
-    public static double multiply (double num1 ,doublw num2){
+    public static double multiply (double num1 ,double num2){
         return num1*num2;
 }
 
